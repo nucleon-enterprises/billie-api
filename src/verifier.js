c0404db4194bfdf97f3dea2faaba148fd80a9a2c
cl3t0 <cl3t0.sh@gmail.com>
2019-07-22 16:12:17 -0300

    Code beautified in verified.js

--- src/verifier.js
+++ src/verifier.js
@@ -15,34 +15,55 @@ module.exports = (req, res) => {
 
     let accessToken = req.body.accessToken;
     let clientModule = cModules.findOne({ accessToken }).name;
+
     if (!accessToken || !clientModule) {
+
         logIt("wrong credentials");
+
     } else {
+
         let command = req.body.command;
         let type = req.body.type;
+
         if (!command || !type) {
+
             logIt("missing parameters");
+
         } else if (!allowedCommandTypes.includes(type)) {
+
             logIt("type isn't allowed");
+
         } else if (type == 'pure') {
+
             let moduleName = command.module;
             let func = command.func;
             let args = command.args;
+
             let chosenModule = aModules.findOne({ name: moduleName });
+
             if (!chosenModule.name) {
+
                 logIt("module doesn't exist");
+
             } else {
+
                 let modulePort = chosenModule.port;
                 let accessToken = chosenModule.accessToken;
                 let address = chosenModule.address;
+
                 let query = { accessToken, command: { func, args }};
+
                 axios.post(`${address}:${modulePort}`, query)
                 .then((response) => {
                     logIt(JSON.stringify(response, null, 4));
                 });
+
             }
+            
         } else {
+
             logIt("Billie for now only receive pure commands");
+
         }
     }
 };
